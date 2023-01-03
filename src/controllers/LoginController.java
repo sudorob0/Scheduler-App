df35e8ce93b54fe3eaefe527a6b29e1d39ec0af5
Robert Uhl <80855216+sudorob0@users.noreply.github.com>
2023-01-02 21:03:05 -0700

    created add customer and appointment screens

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,4 +1,12 @@
 package controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public TextField usernameTextField;
+    public TextField passwordTextField;
+    public TextField locationTextField;
+    public Button loginButton;
+    public Button cancelButton;
 }
