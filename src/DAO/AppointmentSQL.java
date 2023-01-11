3c5ab8faecba37b95242a97d2765e7f878bfcb7e
Robert Uhl <80855216+sudorob0@users.noreply.github.com>
2023-01-10 21:11:10 -0700

    frontend add appointment is complete

--- src/DAO/AppointmentSQL.java
+++ src/DAO/AppointmentSQL.java
@@ -3,7 +3,6 @@ package DAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import models.Appointment;
-import models.Contact;
 
 import java.sql.*;
 
@@ -11,6 +10,12 @@ import java.sql.PreparedStatement;
 import java.time.LocalDateTime;
 
 public class AppointmentSQL {
+    /**
+     * This method makes sql queries and returns a list of appointment objects
+     * @param sqlQuery provide the sequel query to be run on the sql data base
+     * @return list of appointment objects
+     * @throws SQLException for sql errors
+     */
     public static ObservableList<Appointment> makeAppointmentQuery(String sqlQuery) throws SQLException {
         ObservableList<Appointment> appointmentsList = FXCollections.observableArrayList();
         PreparedStatement ps = DBConnection.getConnection().prepareStatement(sqlQuery);
@@ -33,7 +38,7 @@ public class AppointmentSQL {
     }
 
     public static ObservableList<Appointment> getAllAppointments() throws SQLException {
-        ObservableList<Appointment> appointmentList = makeAppointmentQuery("SELECT * FROM appointments");
+        ObservableList<Appointment> appointmentList = makeAppointmentQuery("SELECT * FROM appointments ORDER BY Appointment_ID");
         return appointmentList;
     }
 
@@ -48,14 +53,25 @@ public class AppointmentSQL {
     }
 
     public static void deleteAppointment(int appointmentID) throws SQLException {
-        String deleteStatement = "DELETE from appointments WHERE Appointment_ID=" +appointmentID;
+        String deleteStatement = "DELETE from appointments WHERE Appointment_ID=" + appointmentID;
         PreparedStatement ps = DBConnection.getConnection().prepareStatement(deleteStatement);
         ps.execute();
     }
 
-    public static boolean addAppointment(String contactName, String title, String location, String type, LocalDateTime start, LocalDateTime end, Integer customerId, Integer userID) throws SQLException {
+    public static boolean addAppointment(String contactName, String title, String location, String type, Integer customerId, Integer userID, LocalDateTime start, LocalDateTime end) throws SQLException {
         Integer contactID = ContactSQL.getContactID(contactName);
         System.out.print(contactID);
         return Boolean.TRUE;
+    }
+
+    /**
+     * generates an appointment id by retrieving a list ordered by appointment ids and the adding 1 to the last id in the list
+     * @return appointment id integer
+     * @throws SQLException for sql errors
+     */
+    public static int generateAppointmentID() throws SQLException {
+        ObservableList<Appointment> appointmentList = getAllAppointments();
+        Integer newAppointmentID = appointmentList.get(-1).getAppointmentID() + 1;
+        return newAppointmentID;
     }
 }
--- src/controllers/AddAppointmentController.java
+++ src/controllers/AddAppointmentController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import DAO.AppointmentSQL;
 import DAO.ContactSQL;
 import DAO.CustomerSQL;
 import DAO.UserSQL;
@@ -8,10 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import models.Contact;
 import models.Customer;
 import models.User;
@@ -31,7 +27,6 @@ public class AddAppointmentController implements Initializable {
     public TextField appointmentidTextField;
     public TextField titleTextField;
     public TextField locationTextField;
-    public TextField descriptionTextField;
     public ComboBox useridComboBox;
     public ComboBox customeridComboBox;
     public Button addButton;
@@ -42,6 +37,7 @@ public class AddAppointmentController implements Initializable {
     public DatePicker endDatePicker;
     public ComboBox startTimeComboBox;
     public ComboBox endTimeComboBox;
+    public TextArea descriptionTextArea;
 
     public void initialize(URL url, ResourceBundle resourceBundle){
         // Populates the type combo box
@@ -111,7 +107,20 @@ public class AddAppointmentController implements Initializable {
         else if (customeridComboBox.getSelectionModel().getSelectedItem() == null){PopUpBox.errorBox("The customer ID field must be filled out to continue");}
         else if (typeComboBox.getSelectionModel().getSelectedItem() == null){PopUpBox.errorBox("The appointment type field must be filled out to continue");}
         else if (contactComboBox.getSelectionModel().getSelectedItem() == null){PopUpBox.errorBox("The contact field must be filled out to continue");}
+        else if (startDatePicker.getValue() == null){PopUpBox.errorBox("The start date field must be filled out to continue");}
+        else if (startTimeComboBox.getSelectionModel().getSelectedItem() == null){PopUpBox.errorBox("The start time field must be filled out to continue");}
+        else if (endTimeComboBox.getSelectionModel().getSelectedItem() == null){PopUpBox.errorBox("The end time field must be filled out to continue");}
         else{
+            // If no end date is provided then use the start date
+            if (endDatePicker.getValue() == null){endDatePicker.setValue(startDatePicker.getValue());}
+            // Save full start and end date/times to one variable
+            LocalDateTime enteredStartDT = LocalDateTime.of(startDatePicker.getValue(), LocalTime.parse((CharSequence) startTimeComboBox.getSelectionModel().getSelectedItem()));
+            LocalDateTime enteredEndDT = LocalDateTime.of(endDatePicker.getValue(),  LocalTime.parse((CharSequence) endTimeComboBox.getSelectionModel().getSelectedItem()));
+            // Verify end date/time is after start date/time
+            if (enteredStartDT.isAfter(enteredEndDT)){
+                PopUpBox.errorBox("The start date/time has to be before the end data/time");
+            }
+            System.out.print("That it the correct format!");
 
         }
 
@@ -119,15 +128,14 @@ public class AddAppointmentController implements Initializable {
 
         String enteredTitle = titleTextField.getText();
         String enteredLocation = locationTextField.getText();
-        String enteredDescription = descriptionTextField.getText();
         Integer enteredUserID = (Integer) useridComboBox.getSelectionModel().getSelectedItem();
         Integer enteredCustomerID = (Integer) customeridComboBox.getSelectionModel().getSelectedItem();
         String enteredType = (String) typeComboBox.getSelectionModel().getSelectedItem();
         String enteredContact = (String) contactComboBox.getSelectionModel().getSelectedItem();
         //LocalTime enteredStartTime = (LocalTime) startTimeComboBox.getValue();
         //LocalTime enteredEndTime = (LocalTime) endTimeComboBox.getValue();
-        //LocalDateTime enteredStartDT = LocalDateTime.of(startDatePicker.getValue(), enteredStartTime);
-        //LocalDateTime enteredEndDT = LocalDateTime.of(endDatePicker.getValue(), enteredEndTime);
+        //LocalDateTime enteredStartDT = LocalDateTime.of(startDatePicker.getValue(), (LocalTime) startTimeComboBox.getValue());
+        //LocalDateTime enteredEndDT = LocalDateTime.of(endDatePicker.getValue(), (LocalTime) endTimeComboBox.getValue());
 
         //AppointmentSQL.addAppointment("'Daddy Warbucks'");
     }
